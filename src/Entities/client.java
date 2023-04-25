018c8a434ebd2cd83ecfa0921292dc0fbaceb1e8
balha123 <bensaad.hedi@esprit.tn>
2023-04-25 11:00:16 +0100

    ....

--- src/Entities/client.java
+++ src/Entities/client.java
@@ -15,8 +15,6 @@ public class client extends User {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
-
     public String getDomaine() {
         return domaine;
     }
